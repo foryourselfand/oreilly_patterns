5fa0b6685178b8a5efd4f57abb1e640426e85458
foryourselfand <foryourselfand@gmail.com>
2018-11-03 01:07:36 +0300

    Ducks

---
+++ src/Ch1/MallardDuck.java
@@ -0,0 +1,16 @@
+package Ch1;
+
+import Ch1.Fly.FlyWithWings;
+import Ch1.Quack.Quack;
+
+public class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm real Mallard duck");
+    }
+}
