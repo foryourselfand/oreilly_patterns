c3ba4236aff43ada281396fac91bb84f13d89ad9
foryourselfand <foryourselfand@gmail.com>
2018-11-03 01:09:04 +0300

    Simulator

---
+++ src/Ch1/MiniDuckSimulator.java
@@ -0,0 +1,9 @@
+package Ch1;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallardDuck = new MallardDuck();
+        mallardDuck.perfomQuack();
+        mallardDuck.perfomFly();
+    }
+}
