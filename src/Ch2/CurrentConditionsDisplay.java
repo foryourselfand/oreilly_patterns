11323f196f4193227bbdf2d79693585731707d35
foryourselfand <foryourselfand@gmail.com>
2018-11-03 01:32:10 +0300

    Display

---
+++ src/Ch2/CurrentConditionsDisplay.java
@@ -0,0 +1,36 @@
+package Ch2;
+
+import Ch2.Interfaces.DisplayElement;
+import Ch2.Interfaces.Observer;
+import Ch2.Interfaces.Subject;
+
+public class CurrentConditionsDisplay implements Observer,
+        DisplayElement {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println(
+                "Current conditions: " + temperature +
+                        "F degrees and " + humidity +
+                        "% humidity and " + pressure +
+                        " pressure");
+    }
+}
