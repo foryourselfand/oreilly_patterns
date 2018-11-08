47fca12253c97729f9e748de9f94f6f0f8df94ed
foryourselfand <foryourselfand@gmail.com>
2018-11-08 16:29:28 +0300

    Pizzas

---
+++ src/Ch4_Factory/AbstractFactory/Pizzas/CheesePizza.java
@@ -0,0 +1,18 @@
+package Ch4_Factory.AbstractFactory.Pizzas;
+
+import Ch4_Factory.AbstractFactory.Factories.PizzaIngredientFactory;
+
+public class CheesePizza extends Pizza {
+    PizzaIngredientFactory ingredientFactory;
+
+    public CheesePizza(PizzaIngredientFactory ingredientFactory) {
+        this.ingredientFactory = ingredientFactory;
+    }
+
+    void prepare() {
+        System.out.println("Preparing " + name);
+        dough = ingredientFactory.createDough();
+        sauce = ingredientFactory.createSauce();
+        cheese = ingredientFactory.createCheese();
+    }
+}
