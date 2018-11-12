99b399a72f8ab1b5404b1a0830b3289b8efb0be8
foryourselfand <foryourselfand@gmail.com>
2018-11-12 22:15:53 +0300

    Composite example

---
+++ src/Ch9_Composite/Menu/Waitress.java
@@ -0,0 +1,13 @@
+package Ch9_Composite.Menu;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
