cfded5a56b8411260d59b7579beddfef7be1909f
foryourselfand <foryourselfand@gmail.com>
2018-11-13 22:24:34 +0300

    Composite and Iterator

--- src/Ch12_Composite/DuckSimulator.java
+++ src/Ch12_Composite/DuckSimulator.java
@@ -10,19 +10,46 @@ public class DuckSimulator {
     }
 
     void simulate(AbstractDuckFactory duckFactory) {
-        Quackable mallardDuck = duckFactory.createMallardDuck();
         Quackable redheadDuck = duckFactory.createRedheadDuck();
         Quackable duckCall = duckFactory.createDuckCall();
         Quackable rubberDuck = duckFactory.createRubberDuck();
+        Quackable mallardDuck = duckFactory.createMallardDuck();
         Quackable gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("Duck Simulator");
+        System.out.println("\nDuck Simulator: With Composite - Flocks");
+
+        Flock flockOfDucks = new Flock();
+
+        flockOfDucks.add(redheadDuck);
+        flockOfDucks.add(duckCall);
+        flockOfDucks.add(rubberDuck);
+        flockOfDucks.add(gooseDuck);
+
+        Flock flockOfMallards = new Flock();
+
+        Quackable mallardOne = duckFactory.createMallardDuck();
+        Quackable mallardTwo = duckFactory.createMallardDuck();
+        Quackable mallardThree = duckFactory.createMallardDuck();
+        Quackable mallardFour = duckFactory.createMallardDuck();
+
+        flockOfMallards.add(mallardOne);
+        flockOfMallards.add(mallardTwo);
+        flockOfMallards.add(mallardThree);
+        flockOfMallards.add(mallardFour);
+
+        flockOfDucks.add(flockOfMallards);
 
         simulate(mallardDuck);
         simulate(redheadDuck);
         simulate(duckCall);
         simulate(rubberDuck);
         simulate(gooseDuck);
+
+        System.out.println("\nDuck Simulator: Whole Flock Simulation");
+        simulate(flockOfDucks);
+
+        System.out.println("\nDuck Simulator: Mallard Flock Simulator");
+        simulate(flockOfMallards);
 
         System.out.println("The ducks quacked "
                 + QuackCounter.getQuacks() + " times");
