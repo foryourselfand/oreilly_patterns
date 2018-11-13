de820fe92db12adb6ae8bdc1002536508f127699
foryourselfand <foryourselfand@gmail.com>
2018-11-13 12:53:20 +0300

    Winner state and Test Drive

--- src/Ch10_State/WithState/States/HasQuarterState.java
+++ src/Ch10_State/WithState/States/HasQuarterState.java
@@ -2,7 +2,10 @@ package Ch10_State.WithState.States;
 
 import Ch10_State.WithState.GumballMachine;
 
+import java.util.Random;
+
 public class HasQuarterState implements State {
+    Random randomWinner = new Random(System.currentTimeMillis());
     GumballMachine gumballMachine;
 
     public HasQuarterState(GumballMachine gumballMachine) {
@@ -20,7 +23,12 @@ public class HasQuarterState implements State {
 
     public void turnCrank() {
         System.out.println("You turned...");
-        gumballMachine.setState(gumballMachine.getSoldState());
+        int winner = randomWinner.nextInt(10);
+        if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+            gumballMachine.setState(gumballMachine.getWinnerState());
+        } else {
+            gumballMachine.setState(gumballMachine.getSoldState());
+        }
     }
 
     public void dispense() {
