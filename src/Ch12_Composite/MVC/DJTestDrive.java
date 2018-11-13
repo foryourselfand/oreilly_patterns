3cd6ff019d0ab6f0eebb4db057ec23518dad834f
foryourselfand <foryourselfand@gmail.com>
2018-11-13 23:35:46 +0300

    Ez added hear

--- src/Ch12_Composite/MVC/DJTestDrive.java
+++ src/Ch12_Composite/MVC/DJTestDrive.java
@@ -1,7 +1,8 @@
 package Ch12_Composite.MVC;
 
 public class DJTestDrive {
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         BeatModelInterface model = new BeatModel();
         ControllerInterface controller = new BeatController(model);
     }
